4bb42eba8c3c82b6fbf93f28845407756126a4fe
cmangan <coreymangan22@live.ie>
2018-06-28 12:26:47 +0100

    fixed errors

--- Back_end/models/index.js
+++ Back_end/models/index.js
@@ -2,6 +2,7 @@
 
 const Sequelize = require('sequelize');
 const path = require('path');
+const fs = require('fs');
 const basename = path.basename(__filename);
 const db = {};
 
@@ -15,7 +16,7 @@ const sequelize = new Sequelize(CONFIG.db_name, CONFIG.db_user, CONFIG.db_passwo
 
 fs.readdirSync(__dirname).filter(file => {
     return (file.indexOf('.' !== 0) && (file !== basename) && (file.slice(-3) === '.js'));
-}).foreach(file => {
+}).forEach(file => {
     var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
 });
@@ -25,3 +26,8 @@ Object.keys(db).forEach(modelName => {
         db[modelName].associate(db);
     }
 });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
